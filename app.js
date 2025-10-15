c64aa9c34c4141221020b7f75b32e406c53877cb
clayscode11 <bartlettclayton0@gmail.com>
2025-10-14 20:40:06 -0700

    save, render, etc functions created

--- app.js
+++ app.js
@@ -13,7 +13,47 @@ function closeNoteDialog() {
   document.getElementById("noteDialog").close();
 }
 
+function saveNote(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("noteTitle").value.trim();
+  const content = document.getElementById("noteContent").value.trim();
+
+  notes.unshift({
+    id: generateId,
+    title: title,
+    content: content,
+  });
+
+  saveNotes();
+}
+
+function generateId() {
+  return Date.now().toString();
+}
+
+function saveNotes() {
+  localStorage.setItem("Synapses", JSON.stringify(notes));
+}
+
+function renderNotes() {
+  const notesContainer = document.getElementById("notesContainer");
+
+  if (notes.length === 0) {
+    notesContainer.innerHTML = `
+    <div class="empty-state">
+      <h2>No Notes Yet</h2>
+      <p>Create your first notes to get started!</p>
+      <button class="add-note-button" onclick="openNoteDialog()">+Add Your First Note</button>
+    </div>
+    `;
+    return;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("noteForm").addEventListener("submit", saveNote);
+
   document
     .getElementById("noteDialog")
     .addEventListener("click", function (event) {
